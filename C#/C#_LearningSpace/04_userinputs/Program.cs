5702a7e254517da8e37f57e8ec39fea7a59e52da
dangray1125 <dangray1125@gmail.com>
2020-12-06 08:09:33 -0500

    Further adding of the learning material

---
+++ C#/C#_LearningSpace/04_userinputs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04_userinputs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();               // Takes the user input and stores it as a string.
+            Console.WriteLine("Your name is {0}",name);
+
+
+            Console.WriteLine("Enter a character symbol to recieve its ASCII code value.");
+            int asciiValue = Console.Read();                              // Takes the user input and converts it to an interger equal to its ascii equivlent
+            Console.WriteLine("The ASCII code of the letter you input is: {0}",asciiValue);
+        }
+    }
+}
---
+++ C#/C#_LearningSpace/05_dataConversion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05_dataConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {     
+            double myDouble = 69.5942;
+            int myInt;
+
+            myInt = (int)myDouble;      //EXPLICIT casting.
+            Console.WriteLine("myDouble was explicitly casted to int.  " + myInt);
+            
+            float myFloat =35.76F;
+            myDouble = myFloat;  //implicit casting
+            Console.WriteLine("mydouble was implicitly casted to an int "+ myDouble);
+        
+            string myString = myDouble.ToString();
+            string my2String = myFloat.ToString();
+
+            Console.WriteLine("my2String is a type cast from a float to a string. "+ my2String);
+            Console.Read();
+
+        }
+    }
+}
