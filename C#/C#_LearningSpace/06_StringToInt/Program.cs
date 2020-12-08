b7929a41d8adc253a5a52238b27257d152016023
dangray1125 <dangray1125@gmail.com>
2020-12-07 23:08:12 -0500

    Updating Exercises

---
+++ C#/C#_LearningSpace/06_StringToInt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06_StringToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string userNumber;
+            userNumber = Console.ReadLine();
+            int num = 0;
+            num = Int32.Parse(userNumber);
+            Console.WriteLine("{0} has been converted from a string to a number",num);
+            Console.Read();
+        }
+
+    }
+}
