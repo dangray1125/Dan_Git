a048aac0928e528f5776f698a6d08a83c77c6256
Dan <dangray1125@gmail.com>
2021-02-25 19:31:15 -0500

    update

---
+++ C#/C#_LearningSpace/18_tearnary_operator_Challenge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _18_tearnary_operator_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = -5;
+            string stateOfMatter;
+
+            stateOfMatter = temperature < 0 ? "solid" : "liquid";
+
+            Console.WriteLine("State of matter is {0}", stateOfMatter);
+            Console.ReadKey();
+        }
+    }
+}
